9d1c3b1a35d8dc6f8e817b76c25e033bd51f7dcc
kfwang177880 <kfwang177880@yahoo.com>
2020-08-31 13:00:53 -0400

    add seed.sql

--- seed.sql
+++ seed.sql
@@ -7,7 +7,6 @@ INSERT INTO user (name,imageUrl,address, email, password) VALUES ('Alphard','htt
 INSERT INTO user (name,address, email, password) VALUES ('Baruffio','20 W Main St Bloomsburg, PA 17815','Baruffio@yahoo.com','My$try9u#st');
 INSERT INTO user (name,imageUrl,address, email, password) VALUES ('Hbeery','https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807__340.png','6320 Golden Triangle Dr, Greenbelt, MD 20770-3230','Hbeery@msn.com','Uwi1ln#v3r');
 
-
 INSERT INTO item (name, price, description, imageUrl) VALUES ('Red',80,'A super premium-quality, 100% acrylic interior coating, formulated using our proprietary Color Lock® Technology.','https://media4.picsearch.com/is?hWdrry4gwxKi_sfcTI1dPQcsMo4XEhXAt7yCeNiPLFQ&height=341');
 INSERT INTO item (name, price, description, imageUrl) VALUES ('Blue',43,'A premium-quality acrylic latex paint designed for easy application to a wide variety of interior surfaces.','https://media5.picsearch.com/is?AM821UvYZrDGXfmmH4dqpvuVltIBmwjMM9mb8PaPyCQ&height=227');
 INSERT INTO item (name, price, description) VALUES ('Green',64,'A premium-quality paint which adheres powerfully to exterior substrates. With easy application by brush, roller, or spray, this coating delivers a durable finish. Exterior paint may require additional time for pick up availability.');
