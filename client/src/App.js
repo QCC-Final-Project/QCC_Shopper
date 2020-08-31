44304893b81bab895554120ebbe31e0b16c91aa5
Michael <michael.jim.harris@gmail.com>
2020-08-31 11:13:01 -0400

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; //not relevant to our project yet
 import Students from './students'
-import {Navbar, Index} from './Components/Index';
-import { Router } from 'react-router-dom'
+import Routes from './Routes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Navbar } from './Components';
 
-function App() {
-  return (
-    <div></div>
-  );
-}
+class QCC_Shopper extends Component {
 
-export default App;
+  //don't think we need this yet since we don't need state atm
+  constructor() {
+    super();
+  };
+
+  render() {
+    return(
+      <Router>
+        <Navbar />
+        <Routes />
+      </Router>
+    );
+  }
+};
+
+export default QCC_Shopper
