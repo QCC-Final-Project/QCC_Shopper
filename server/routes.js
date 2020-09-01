03ce4918328647b9fd26e40516a970f72a7ada9e
Michael <michael.jim.harris@gmail.com>
2020-09-01 10:08:03 -0400

    resolved conflicts

--- server/routes.js
+++ server/routes.js
@@ -2,33 +2,41 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const db = require('./db/db');
 const port = 4000;
+
+let users = [
+    { name: 'Plato', id: 1 },
+    { name: 'Copernicus', id: 2 },
+  ];
+
 //Middleware
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-let users = [
-  { name: 'Plato', id: 1 },
-  { name: 'Copernicus', id: 2 },
-];
+
 // app.use((req, res, next) => {
 //   console.log(req.method, req.url);
 //   next();
 // });
+
 //joins a path between the client and the server
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-//this will print out on the user page { name: 'Einstein', id: 1 },  { name: 'Kepler', id: 2 },
+
+//this will print out on the user page { name: 'Plato', id: 1 },  { name: 'Copernicus', id: 2 },
 app.get('/users', (req, res, next) => {
   res.send(users);
 });
-//this will push any new user's information into the back of the array
-app.post('/users', (req, res, next) => {
-  const { name } = req.body;
-  const id = users[users.length - 1].id + 1;
-  const newUser = { name, id };
-  users.push(newUser);
-  res.send(newUser);
-});
+
+// //this will push any new user's information into the back of the array
+// app.post('/users', (req, res, next) => {
+//   const { name } = req.body;
+//   const id = users[users.length - 1].id + 1;
+//   const newUser = { name, id };
+//   users.push(newUser);
+//   res.send(newUser);
+// });
+
 //this will access a specific id #'s information.
 app.put('/users/:id', (req, res, next) => {
   const user = users.filter((user) => user.id === parseInt(req.params.id))[0];
@@ -38,17 +46,31 @@ app.put('/users/:id', (req, res, next) => {
   user.name = 'hello';
   res.send(user);
 });
-// this will delete a specific id #'s information
-app.delete('/users/:id', (req, res, next) => {
-  users = users.filter((user) => user.id !== parseInt(req.params.id));
-  res.send(users);
-});
+
+// // this will delete a specific id #'s information
+// app.delete('/users/:id', (req, res, next) => {
+//   users = users.filter((user) => user.id !== parseInt(req.params.id));
+//   res.send(users);
+// });
+
 // Error message (routes that are not found)
 app.use((req, res, next) => {
   console.log('hello');
   res.send('404 not found');
 });
+
 //Linking the specific port
 app.listen(port, () => {
     console.log(`Server Started on Port: ${port}`);
 });
+
+//sync database
+const startApp = async () => {
+    const PORT = 4000;
+
+    await db.sync({force: true});
+
+    app.listen(PORT,() => {
+        console.log(`listening on port ${PORT}`);
+    });
+};
