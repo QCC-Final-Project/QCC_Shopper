a73d36c13326a6ecc8104a705e2ddf9b08394735
Michael <michael.jim.harris@gmail.com>
2020-09-02 11:21:11 -0400

    ListItems worked on

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,4 +1,4 @@
-export {default as ListItems} from './ListItems';
 export {default as Home} from './Home';
-export {default as Navbar} from './Navbar';
+export {default as ListItems} from './ListItems';
 export {default as Cart} from './Cart';
+export {default as Navbar} from './Navbar';
